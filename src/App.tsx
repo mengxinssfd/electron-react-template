b281d59b629929a346816ab266efd936df3158eb
DYH_HP <xinzon32177@163.com>
2026-05-05 19:51:42 +0800

    chore: init react vite electron tailwind

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1 className="font-bold text-2xl text-amber-950">Hello World!</h1>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { router } from './router';
+import React from 'react';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement,
+);
+root.render(<React.StrictMode> <RouterProvider router={router}></RouterProvider></React.StrictMode>);
